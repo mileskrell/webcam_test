82f2e76a267dfc688672562631d955c81dca51eb
Miles Krell <noreply@mileskrell.com>
2020-01-09 13:26:26 -0500

    Add Google STUN server

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ async function main() {
     localStream = await navigator.mediaDevices.getUserMedia({video: true});
     localVideo.srcObject = localStream;
 
-    rtcPeerConnection = new RTCPeerConnection();
+    rtcPeerConnection = new RTCPeerConnection({iceServers: [{urls: "stun:stun.l.google.com:19302"}]});
     localStream.getTracks().forEach(track => rtcPeerConnection.addTrack(track));
 }
 
